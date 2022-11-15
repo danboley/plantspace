336b9a713f26dd7b1185a80e153460151f9659b4
Jim <jimba.ed@outlook.com>
2022-11-15 15:47:43 -0500

    made more changes to the nav bar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 function NavBar (){
     return (
-        <nav>
-            <NavLink exact to = "/">Home</NavLink>
-            <NavLink exact to = "/PlantContainer">Your Plants</NavLink>
-            <NavLink exact to = "/NewPlantForm">Add a New Plant</NavLink>
-            <NavLink exact to = "/login">Login/SignUp</NavLink>
+        <nav className="navigation">
+            <NavLink className={"navbtn"} exact to = "/">Home</NavLink>
+            <NavLink className={"navbtn"} exact to = "/PlantContainer">Your Plants</NavLink>
+            <NavLink className={"navbtn"} exact to = "/NewPlantForm">Add a New Plant</NavLink>
+            <NavLink className={"navbtn"} exact to = "/login">Login/SignUp</NavLink>
         </nav>
     )
 }
--- client/src/Components/PrivatePlantCard.js
+++ client/src/Components/PrivatePlantCard.js
@@ -8,12 +8,12 @@ function PrivatePlantCard (){
     function handleDelete(){
         console.log("DELETE")
     }
-    
+
     return (
         <div className = "priCard">
             <div> Name for Your Plant: </div>
             <div>Type of Plant: </div>
-            <img alt="placeholder" src="https://www.cnet.com/a/img/resize/18745075acf9e4ed11171d3398f09c29733866f3/hub/2022/10/15/4ee63290-986c-42c1-af30-1ab0e5ca6dd0/newemoji.png?auto=webp&width=1200"></img>
+            <img className="priImage" alt="placeholder" src="https://www.cnet.com/a/img/resize/18745075acf9e4ed11171d3398f09c29733866f3/hub/2022/10/15/4ee63290-986c-42c1-af30-1ab0e5ca6dd0/newemoji.png?auto=webp&width=1200"></img>
             <div>Last Watered: </div>
             <button onClick={handleWater}>Need Water</button>
             <button onClick={handleDelete}>Delete Plant</button>
