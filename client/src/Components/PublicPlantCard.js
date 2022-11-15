319231dd0102d8ffd94f1b669a83e896ea7ca58a
Tristen <tristen.hammon21@gmail.com>
2022-11-15 14:35:52 -0600

    yo

--- client/src/Components/PublicPlantCard.js
+++ client/src/Components/PublicPlantCard.js
@@ -4,7 +4,6 @@ function PublicPlantCard (){
         <div>
             <div>NAme the Category</div>
             <div>Owner:</div>
-            <img>Picture</img>
             <div>Where You Can Find a Plant like Me: </div>
         </div>
     )
